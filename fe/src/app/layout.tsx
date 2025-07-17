b9c434ffe45ceb33d6da530cb2335f62724a5034
I hate iu <tranhung10122003@gmail.com>
2025-07-17 08:38:03 +0700

    feat: init FE

---
+++ fe/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import { QueryProvider } from '@/providers/query-provider'
+import './globals.css'
+
+export default function RootLayout({
+                                       children,
+                                   }: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+        <body>
+        <QueryProvider>
+            {children}
+        </QueryProvider>
+        </body>
+        </html>
+    )
+}
