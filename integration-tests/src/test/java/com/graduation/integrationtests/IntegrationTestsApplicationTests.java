16408c3adc18d87fcd1c8792fb82879ad80c7865
I hate iu <tranhung10122003@gmail.com>
2025-07-30 01:19:09 +0700

    feat: init integration test

---
+++ integration-tests/src/test/java/com/graduation/integrationtests/IntegrationTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graduation.integrationtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
